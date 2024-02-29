5a55293603440389075ce7f7118afb1a52dc05cc
Asleepdoggy <maomaoovo@outlook.com>
2024-02-29 08:50:13 +0800

    QAQ

--- N11_GENERAL_TIMER_PWM_SERVO/Drivers/BSP/GTIM.c
+++ N11_GENERAL_TIMER_PWM_SERVO/Drivers/BSP/GTIM.c
@@ -11,7 +11,7 @@ TIM_HandleTypeDef g_timx_pwm_chy_handle;
  */
 void g_timx_pwm_chy_init(uint16_t arr,uint16_t psc)
 {
-	TIM_OC_InitTypeDef timx_oc_pwm_chy;
+	TIM_OC_InitTypeDef timx_oc_pwm_chy = {0};
 	
 	g_timx_pwm_chy_handle.Instance = TIM3;
 	g_timx_pwm_chy_handle.Init.Prescaler = psc;                      /*预分频系数*/
@@ -21,7 +21,7 @@ void g_timx_pwm_chy_init(uint16_t arr,uint16_t psc)
 	
 	timx_oc_pwm_chy.OCMode = TIM_OCMODE_PWM1;                        /* 模式选择PWM1 */
 	timx_oc_pwm_chy.Pulse = arr/2;                                   /* 设置比较值,此值用来确定占空比 */
-	timx_oc_pwm_chy.OCNPolarity = TIM_OCPOLARITY_LOW;
+	timx_oc_pwm_chy.OCNPolarity = TIM_OCPOLARITY_HIGH;
 	HAL_TIM_PWM_ConfigChannel(&g_timx_pwm_chy_handle,&timx_oc_pwm_chy,TIM_CHANNEL_2);
 	HAL_TIM_PWM_Start(&g_timx_pwm_chy_handle,TIM_CHANNEL_2);
 }
@@ -39,9 +39,10 @@ void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim)
 		gpio_init_struct.Mode = GPIO_MODE_AF_PP;
 		gpio_init_struct.Pull = GPIO_PULLUP;
 		gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;
-		HAL_GPIO_Init(GPIOB,&gpio_init_struct);
+		HAL_GPIO_Init(GPIOA,&gpio_init_struct);
 		
+		/*重定义功能才需要写这个，默认复用功能不用
 		__HAL_RCC_AFIO_CLK_ENABLE();
-		__HAL_AFIO_REMAP_TIM3_PARTIAL();
+		__HAL_AFIO_REMAP_TIM3_PARTIAL();*/
 	}
 }
